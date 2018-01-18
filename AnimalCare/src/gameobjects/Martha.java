c614d2444381b6e64c83310bc73a9d0645b6d34a
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-01-18 12:15:34 -0500

    Added all the characters.That's all folks!

---
+++ AnimalCare/src/gameobjects/Martha.java
@@ -0,0 +1,11 @@
+package gameobjects;
+
+public class Martha extends Character {
+
+	public Martha(String charName, String charGreeting, String charImgPath, int charHealth, int charHunger,
+			int charCleanliness, String[] greetingArray, int daysAlive) {
+		super(charName, charGreeting, charImgPath, charHealth, charHunger, charCleanliness, greetingArray, daysAlive);
+		// TODO Auto-generated constructor stub
+	}
+
+}
