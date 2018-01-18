9ea7889bb589ccd21647bfc35ce44259b090ff18
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-01-18 11:59:16 -0500

    Finished the Character class.

--- AnimalCare/src/gameobjects/Character.java
+++ AnimalCare/src/gameobjects/Character.java
@@ -1,87 +1,95 @@
 package gameobjects;
 
 public abstract class Character {
-	private static String charName;
-	private static String charGreeting;
-	private static String charImgPath;
-	private static int charHealth;
-	private static int charHunger;
-	private static int charCleanliness;
-	private static String[] greetingArray;
-	private static int daysAlive;
+	private String charName;
+	private String charGreeting;
+	private String charImgPath;
+	private int charHealth;
+	private int charHunger;
+	private int charCleanliness;
+	private String[] greetingArray;
+	private int daysAlive;
 	
-	public Character(String charName,String charGreeting, String charImagePath, int charHealth, int charHunger, int charCleanliness, String[] greetingArray, int daysAlive) 
+	public Character(String charName,String charGreeting, String charImgPath, int charHealth, int charHunger, int charCleanliness, String[] greetingArray, int daysAlive) 
 	{
-		Character.charName=charName;
-		this.charGreeting=charGreeting;
-		this.charImgPath=charImgPath;
-		this.charHealth=charHealth;
-		this.charHunger=charHunger;
-		this.charCleanliness=charCleanliness;
+		this.setCharName(charName);
+		this.setCharGreeting(charGreeting);
+		this.setCharImgPath(charImgPath);
+		this.setCharHealth(charHealth);
+		this.setCharHunger(charHunger);
+		this.setCharCleanliness(charCleanliness);
+		this.setGreetingArray(greetingArray);
+		this.setDaysAlive(daysAlive);
 	}
 
-	public static String getCharName() {
+	public String getCharName() {
 		return charName;
 	}
 
-	public static void setCharName(String charName) {
-		Character.charName = charName;
+	public void setCharName(String charName) {
+		this.charName = charName;
 	}
 
-	public static String getCharGreeting() {
+	public String getCharGreeting() {
 		return charGreeting;
 	}
 
-	public static void setCharGreeting(String charGreeting) {
-		Character.charGreeting = charGreeting;
+	public void setCharGreeting(String charGreeting) {
+		this.charGreeting = charGreeting;
 	}
 
-	public static String getCharImgPath() {
+	public String getCharImgPath() {
 		return charImgPath;
 	}
 
-	public static void setCharImgPath(String charImgPath) {
-		Character.charImgPath = charImgPath;
+	public void setCharImgPath(String charImgPath) {
+		this.charImgPath = charImgPath;
 	}
 
-	public static int getCharHealth() {
+	public int getCharHealth() {
 		return charHealth;
 	}
 
-	public static void setCharHealth(int charHealth) {
-		Character.charHealth = charHealth;
+	public void setCharHealth(int charHealth) {
+		this.charHealth = charHealth;
 	}
 
-	public static int getCharHunger() {
+	public int getCharHunger() {
 		return charHunger;
 	}
 
-	public static void setCharHunger(int charHunger) {
-		Character.charHunger = charHunger;
+	public void setCharHunger(int charHunger) {
+		this.charHunger = charHunger;
 	}
 
-	public static int getCharCleanliness() {
+	public int getCharCleanliness() {
 		return charCleanliness;
 	}
 
-	public static void setCharCleanliness(int charCleanliness) {
-		Character.charCleanliness = charCleanliness;
+	public void setCharCleanliness(int charCleanliness) {
+		this.charCleanliness = charCleanliness;
 	}
 
-	public static String[] getGreetingArray() {
+	public String[] getGreetingArray() {
 		return greetingArray;
 	}
 
-	public static void setGreetingArray(String[] greetingArray) {
-		Character.greetingArray = greetingArray;
+	public void setGreetingArray(String[] greetingArray) {
+		this.greetingArray = greetingArray;
 	}
 
-	public static int getDaysAlive() {
+	public int getDaysAlive() {
 		return daysAlive;
 	}
 
-	public static void setDaysAlive(int daysAlive) {
-		Character.daysAlive = daysAlive;
+	public void setDaysAlive(int daysAlive) {
+		this.daysAlive = daysAlive;
+	}
+
+	public static int decrementStat()
+	{
+		
+		return 0;
 	}
 
 }
