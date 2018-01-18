95491ad41c8b1fb7b19839a33ee337ddf2cec06b
BT_1N3_17 <BT_1N3_17@BT_1N3_17.bktech.local>
2018-01-18 12:30:45 -0500

    added Runner class

---
+++ AnimalCare/src/Runner.java
@@ -0,0 +1,31 @@
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import java.awt.color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Runner {
+
+	public Runner() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
