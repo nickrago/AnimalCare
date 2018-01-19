4e4b2a3fc8ae9b2b8ed61168860ac3208774ef64
Nick Ragovski <32273230+nickrago@users.noreply.github.com>
2018-01-19 13:14:44 -0500

    Updated Runner, adding 5 buttons for each pet

--- AnimalCare/src/frontend/Runner.java
+++ AnimalCare/src/frontend/Runner.java
@@ -10,6 +10,7 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.layout.BorderPane;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -33,46 +34,121 @@ public class Runner extends Application{
 	       primaryStage.setTitle("Hello World!");
 	              
 	       //Creates new button
-	       Button btn = new Button();
+	       Button martha = new Button();
+	       Button amelie = new Button();
+	       Button mimi = new Button();
+	       Button ned = new Button();
+	       Button holden = new Button();
 	       
 	       //Creates text object
 	       Text txt = new Text(0,10,"Button Not Pressed");
 	       
 	       //Sets the text on the button
-	       btn.setText("Press Me");
+	       martha.setText("Choose Martha");
+	       amelie.setText("Choose Amelie");
+	       mimi.setText("Choose Mimi");
+	       ned.setText("Choose Ned");
+	       holden.setText("Choose Holden");
 	      
 	       //Sets the height and width of the button
-	       btn.setPrefHeight(100);
-	       btn.setPrefWidth(100);
+	       martha.setPrefHeight(50);
+	       martha.setPrefWidth(100);
+	       
+	       amelie.setPrefHeight(50);
+	       amelie.setPrefWidth(100);
+	       
+	       mimi.setPrefHeight(50);
+	       mimi.setPrefWidth(100);
+	       
+	       ned.setPrefHeight(50);
+	       ned.setPrefWidth(100);
+	       
+	       holden.setPrefHeight(50);
+	       holden.setPrefWidth(100);
 	       
 	       //Set the location of the button
-	       btn.setLayoutX(50);
-	       btn.setLayoutY(50);
+	       martha.setLayoutX(50);
+	       martha.setLayoutY(50);
+	       
+	       amelie.setLayoutX(200);
+	       amelie.setLayoutY(50);
+	       
+	       mimi.setLayoutX(350);
+	       mimi.setLayoutY(50);
+	       
+	       ned.setLayoutX(500);
+	       ned.setLayoutY(50);
+	       
+	       holden.setLayoutX(650);
+	       holden.setLayoutY(50);
 	       
 	       
-	       primaryStage.setHeight(635);
-	       primaryStage.setWidth(635);
+	       primaryStage.setHeight(500);
+	       primaryStage.setWidth(850);
       
 	       //Tells the button what to do when clicked
-	       btn.setOnAction(new EventHandler<ActionEvent>() {
+	       martha.setOnAction(new EventHandler<ActionEvent>() {
 
 	    	   //The on-click operation
 	           @Override
 	           public void handle(ActionEvent event) {
 	        	   
-	        	   txt.setText("Button Pressed");
+	        	   txt.setText("You Have Selected Martha");
+	           }
+	       });
+	       amelie.setOnAction(new EventHandler<ActionEvent>() {
+
+	    	   //The on-click operation
+	           @Override
+	           public void handle(ActionEvent event) {
+	        	   
+	        	   txt.setText("You Have Selected Amelie!");
+	           }
+	       });
+	       mimi.setOnAction(new EventHandler<ActionEvent>() {
+
+	    	   //The on-click operation
+	           @Override
+	           public void handle(ActionEvent event) {
+	        	   
+	        	   txt.setText("You Have Selected Mimi!");
+	           }
+	       });
+	       ned.setOnAction(new EventHandler<ActionEvent>() {
+
+	    	   //The on-click operation
+	           @Override
+	           public void handle(ActionEvent event) {
+	        	   
+	        	   txt.setText("You Have Selected Ned");
+	           }
+	       });
+	       holden.setOnAction(new EventHandler<ActionEvent>() {
+
+	    	   //The on-click operation
+	           @Override
+	           public void handle(ActionEvent event) {
+	        	   
+	        	   txt.setText("You Have Selected Holden");
 	           }
 	       });
 
 
-		   
+	       BorderPane gameScreen = new BorderPane();
+	       gameScreen.setId("gamescreen");
 	       
 	       //Sets the GUI and adds button to the scene
 	       Group g = new Group();
 	       
-	       g.getChildren().add(btn);
+	       g.getChildren().add(martha);
+	       g.getChildren().add(amelie);
+	       g.getChildren().add(mimi);
+	       g.getChildren().add(ned);
+	       g.getChildren().add(holden);
+	       
 	       g.getChildren().add(txt);
 	       Scene scene = new Scene(g);
+	       
 	       
 	       //Shows
 	       primaryStage.setScene(scene);
