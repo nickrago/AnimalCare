542882899dfbf399d1e1b9a3d3640ec8d6b1e94a
BT_1N3_15 <BT_1N3_15@BT_1N3_15.bktech.local>
2018-01-30 12:03:04 -0500

    put space in between each function

--- AnimalCare/src/backend/Utilities.java
+++ AnimalCare/src/backend/Utilities.java
@@ -7,18 +7,21 @@ public class Utilities
 {
 	public static String[] greetings= {"","","","",""};
 	public static String[] names= {"","","","",""};
+	
 	public void feed(GameCharacter c)
 	{
 		int c1=c.getCharHunger();
 		c1+=10;
 		c.setCharHunger(c1);
 	}
+	
 	public void med(GameCharacter c)
 	{
 		int c1=c.getCharHealth();
 		c1+=10;
 		c.setCharHealth(c1);
 	}
+	
 	public void clean(GameCharacter c)
 	{
 		int c1=c.getCharCleanliness();
