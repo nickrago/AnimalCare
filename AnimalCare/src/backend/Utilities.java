37dd86e508e1ecb55b7b5e5ad9966fa469589561
BT_1N3_18 <BT_1N3_18@BT_1N3_18.bktech.local>
2018-01-31 12:51:21 -0500

    made Greetings and other base stats for each character

--- AnimalCare/src/backend/Utilities.java
+++ AnimalCare/src/backend/Utilities.java
@@ -5,8 +5,11 @@ package backend;
 */
 public class Utilities
 {
-	public static String[] greetings= {"","","","",""};
+	public static String[] greetings= {"Bonjour! Comment tu vas?","Hi! How was your day?","Howdy, sure is a thing being old!","Hey!!! What's up?!!","What's going on, dude?"};
 	public static String[] names= {"Amelie","Martha","Holden","Mimi","Ned"};
+	public static int[] baseHealth= {100, 110, 80, 100, 90};
+	public static int[] baseHunger= {120, 100, 110, 100, 80};
+	public static int[] baseClean= {100, 100, 80, 90, 70};
 	public void feed(GameCharacter c)
 	{
 		int c1=c.getCharHunger();
