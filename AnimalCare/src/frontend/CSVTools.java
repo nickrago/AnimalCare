913691cb415cbd73fb32630ea5fd31b9f1d3672d
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2018-01-31 12:32:00 -0500

    fixed writeToCSV

--- AnimalCare/src/frontend/CSVTools.java
+++ AnimalCare/src/frontend/CSVTools.java
@@ -2,6 +2,7 @@ package frontend;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
@@ -46,16 +47,23 @@ public class CSVTools {
 		//pw.close();
 	}
 	
-	public static void writeToCSV(String info)
+	public static void writeToCSV(String file, String info)
 	{
-		for (String s : info.split(","))
+		try (FileWriter fw = new FileWriter(Paths.get(file).toString(), true);) 
 		{
-			pw.append(s);
-			pw.append(",");
+			for (String string : info.split(",")) 
+			{
+		 		fw.append(string);
+		 		fw.append(',');
+		 	}
+		 	fw.append('\n');
+		 	fw.flush();
+		 	fw.close();
+		} 
+		catch (IOException e) 
+		{
+		 	e.printStackTrace();
 		}
-		pw.append("\n");
-		
-		//implement into runner to constantly push information into the csv
 	}
 	
 	public static List<GameCharacter> readCSV(String file)
